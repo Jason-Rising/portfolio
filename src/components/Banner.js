681698bf359fa52d8e05c9441a05b7c0ad10e5fd
Jason Rising <66422891+Jason-Rising@users.noreply.github.com>
2024-12-20 20:08:01 +1100

    Improve banner background on loading

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from "@mui/material";
 import profile from '../assets/images/profile.jpg';
 import { ReactComponent as DownArrowIcon } from '../assets/icons/down-arrow-outline.svg';
@@ -6,27 +6,38 @@ import SocialIcons from './SocialIcons';
 import LocaleSelect from './LocaleSelect';
 import { useTranslation } from 'react-i18next';
 
+import bannerBackgroundSmall from '../assets/images/bg_small.jpg';
+import bannerBackground from '../assets/images/bg.jpg';
+
 const Banner = ({ onDownArrowClick, isMobile }) => {
   const [t] = useTranslation("global");
-
+  
   return (
-    <section className="banner">
-      <div className="banner-locale">
-        <LocaleSelect />
+    <div className="banner">
+      <div className='banner-background'>
+        <img src={bannerBackgroundSmall}/>
+        <img src={bannerBackground}/>
+        <div className='banner-background-overlay'/>
       </div>
-      <div className='banner-info'>
-      <div className="line-break" />
-      <img className="profile" src={profile} alt="Profile" />
-      <Typography variant="h3">Jason Rising</Typography>
-      <Typography sx={{ opacity: '80%' }} variant="h6">{t("home.job_title")}</Typography>
-      <SocialIcons />
-      </div>
-      {isMobile && (
-        <div className="banner-downarrow-mobile" onClick={onDownArrowClick}>
-          <DownArrowIcon />
+
+      <div className='banner-content'>
+        <div className="banner-locale">
+          <LocaleSelect />
         </div>
-      )}
-    </section>
+        <div className='banner-info'>
+          <div className="line-break" />
+          <img className="profile" src={profile} alt="Profile" />
+          <Typography variant="h3">Jason Rising</Typography>
+          <Typography sx={{ opacity: '80%' }} variant="h6">{t("home.job_title")}</Typography>
+          <SocialIcons />
+        </div>
+        {isMobile && (
+          <div className="banner-downarrow-mobile" onClick={onDownArrowClick}>
+            <DownArrowIcon />
+          </div>
+        )}
+      </div>
+    </div>
   )};
 
 export default Banner;
