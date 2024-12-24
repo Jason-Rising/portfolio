f4d679bfc00c95a260ecd4fea43fbbe980647f7c
Jason Rising <66422891+Jason-Rising@users.noreply.github.com>
2024-12-24 14:45:19 +1100

    Write translations for each project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Button } from "@mui/material";
 import { CSSTransition } from 'react-transition-group';
+import { useTranslation } from 'react-i18next';
 
-const Navbar = ({ activeView, showProjects, setShowProjects }) => (
+const Navbar = ({ activeView, showProjects, setShowProjects }) => {
+  const [t] = useTranslation("projects");
+
+  return (
     <CSSTransition
-        in={activeView === 'main'}
-        unmountOnExit
-        timeout={300}
-        classNames="navbar"
-  >
-    <div className="navbar">
-        <Button className={!showProjects ? "active" : ""} onClick={() => setShowProjects(true)}>
-        Projects
-        </Button>
-        <Button className={showProjects ? "active" : ""} onClick={() => setShowProjects(false)}>
-        Timeline
-        </Button>
-        <div className="underline" style={{ transform: showProjects ? "translateX(0)" : "translateX(100%)" }} />
-    </div>
+    in={activeView === 'main'}
+    unmountOnExit
+    timeout={300}
+    classNames="navbar"
+>
+  <div className="navbar">
+      <Button className={!showProjects ? "active" : ""} onClick={() => setShowProjects(true)}>
+      {t("project.projects")}
+      </Button>
+      <Button className={showProjects ? "active" : ""} onClick={() => setShowProjects(false)}>
+      {t("project.timeline")}
+      </Button>
+      <div className="underline" style={{ transform: showProjects ? "translateX(0)" : "translateX(100%)" }} />
+  </div>
   </CSSTransition>
-);
+  );
+};
 
 export default Navbar;
